68667f67fea2a4e59824fe88f35572b1544782f8
Jose Vazquez <vazquez.jose.f@gmail.com>
2018-08-23 09:26:04 -0400

    putting some of the logic in place to make the todos functional..wip

--- src/App.js
+++ src/App.js
@@ -3,21 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 const todos = [
-  { id: 123, title: 'get milk', isSelected: true },
-  { id: 223, title: 'get bread', isSelected: true },
-  { id: 323, title: 'do laundry', isSelected: true },
-  { id: 423, title: 'pick up kids', isSelected: true },
-  { id: 523, title: 'drop off kids', isSelected: true },
-  { id: 523, title: 'build ridiculous timline program', isSelected: true },
-  { id: 623, title: 'build app', isSelected: true },
-  { id: 723, title: 'deploy app', isSelected: true },
+  { id: 123, title: 'get milk', isSelected: false },
+  { id: 223, title: 'get bread', isSelected: false },
+  { id: 323, title: 'do laundry', isSelected: false },
+  { id: 423, title: 'pick up kids', isSelected: false },
+  { id: 523, title: 'drop off kids', isSelected: false },
+  { id: 524, title: 'build ridiculous timeline program', isSelected: false },
+  { id: 623, title: 'build app', isSelected: false },
+  { id: 723, title: 'deploy app', isSelected: false },
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { todos: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ todos });
+  }
+
+  handleChange(todo) {
+    const selectedTodo = this.state.todos.filter(t => t.id === todo.id);
+
+    // from the start of the one we want to remove
+    // ...state.slice(0, index);
+    // after the deleted one, to the end
+    // ...state.slice(index + 1);
+
+    // const newTodos = [];
+
+    // this.setState({ todos: newTodos });
+  }
+
   render() {
     const todoStyle = {
       border: '1px dashed #7d7d7d',
-      width: 300,
+      width: 200,
       margin: '10px auto',
       padding: 15,
     };
@@ -31,12 +56,13 @@ class App extends Component {
         <p className="App-intro">this is for a test integration with Firebase</p>
 
         <h2>todos:</h2>
-        {todos.map(todo => (
-          <div style={todoStyle}>
-            <input type="checkbox" />
-            <span>{todo.title}</span>
-          </div>
-        ))}
+        {this.state.todos &&
+          this.state.todos.map(todo => (
+            <div style={todoStyle} key={todo.id}>
+              <input type="checkbox" checked={todo.isSelected} onChange={e => this.handleChange(todo, e)} />
+              <span style={{ marginLeft: 10 }}>{todo.title}</span>
+            </div>
+          ))}
       </div>
     );
   }
